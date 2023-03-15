482d10691e66eb56a987392e7f1e703daf2dac59
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-03-15 14:57:59 +0300

    Fixed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -9,4 +9,8 @@ public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
+
+    public static void main(String[] args) {
+        System.out.println(isAllPositiveNumbers(List.of("1", "2", "3", "4")));
+    }
 }
